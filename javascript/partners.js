53178f63d5aeb285406fcd8e89016a77d37ab2c4
Eli-pharell <118910371+Eli-pharell@users.noreply.github.com>
2024-02-02 04:55:27 -0500

    Push

--- javascript/partners.js
+++ javascript/partners.js
@@ -38,3 +38,23 @@ overlay.onclick = ()=>{
 
 var sliderCopy = slide.cloneNode(true);
 document.getElementById("partner-logo-slider").appendChild(sliderCopy);
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
