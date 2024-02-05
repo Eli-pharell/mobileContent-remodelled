0ab8bb10cf24d10189aa489582f523c2c91d00f5
Eli-pharell <118910371+Eli-pharell@users.noreply.github.com>
2024-02-05 10:38:21 -0500

    Update 4.1

--- javascript/service.js
+++ javascript/service.js
@@ -35,3 +35,13 @@ overlay.onclick = ()=>{
 
 }
 
+const form = document.querySelector('.form1');
+const table = document.querySelector('.request-table');
+var inputs = document.querySelectorAll('input');
+console.log(form);
+
+
+form.addEventListener('submit',(e)=>{
+    e.reset();
+})
+
