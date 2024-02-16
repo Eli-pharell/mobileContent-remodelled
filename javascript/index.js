bf3f558272a90b9cae816dfc50f03291ee1dffb6
Eli-pharell <118910371+Eli-pharell@users.noreply.github.com>
2024-02-16 08:46:37 +0000

    push

--- javascript/index.js
+++ javascript/index.js
@@ -37,8 +37,8 @@ var swiper = new Swiper(".mySwiper", {
     counter++
 
       if(counter>3){
-        await loopDelay(3800);
+        await loopDelay(4000);
         counter = 1
       }
 
-  }, 3990);
+  }, 4000);
