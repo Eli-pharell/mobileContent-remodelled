93de8955fad03c2d5fbde73efdbf386d989fd757
Eli-pharell <118910371+Eli-pharell@users.noreply.github.com>
2024-02-19 07:55:12 +0000

    Push

--- javascript/index.js
+++ javascript/index.js
@@ -22,26 +22,3 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-  var counter = 2;
-
-  function loopDelay(ms){
-    
-    return new Promise( 
-        resolve =>{
-            setTimeout(()=>{resolve('')},ms);
-        })
-}
-
-async function reset(){
-   await loopDelay(3000);
-   if(counter>3){
-    counter = 1;
-   }
-}
-
-  setInterval(() => {
-    document.getElementById('welcome-text-image').src = "images/icons/welcome"+counter+".png";
-    counter++
-
-    reset();
-  },3000);
--- javascript/service.js
+++ javascript/service.js
@@ -4,29 +4,6 @@ var inputs = document.querySelectorAll('input');
 console.log(form);
 
 
-var counter = 2;
-
-function loopDelay(ms){
-  
-  return new Promise( 
-      resolve =>{
-          setTimeout(()=>{resolve('')},ms);
-      })
-}
-
-async function reset(){
- await loopDelay(3000);
- if(counter>3){
-  counter = 1;
- }
-}
-
-setInterval(() => {
-  document.getElementById('welcome-text-image').src = "images/icons/welcome"+counter+".png";
-  counter++
-
-  reset();
-}, 3000);
 
 
 
