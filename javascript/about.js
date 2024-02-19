c526f1253f3037597b66af30f1b474068c8c2be0
Eli-pharell <118910371+Eli-pharell@users.noreply.github.com>
2024-02-19 11:54:06 +0000

    Update 8.0

--- javascript/about.js
+++ javascript/about.js
@@ -3,7 +3,7 @@ var swiper = new Swiper(".swiper", {
 
     breakpoints:{
       1200:{
-        slidesPerView: 3.9,
+        slidesPerView: 3.43,
     spaceBetween: 19,
     loop: false,
 
@@ -25,7 +25,7 @@ var swiper = new Swiper(".swiper", {
 
 
       992:{
-        slidesPerView:2.744,
+        slidesPerView:2.82,
         spaceBetween: 19,
         loop: false,
     
@@ -132,3 +132,34 @@ var swiper = new Swiper(".swiper", {
 
 
   });
+
+
+  var lock = document.querySelectorAll('.fa-lock-open');
+  var text =document.querySelectorAll('.profile-text');
+  var profile = document.querySelectorAll(".profile");
+
+
+  lock.forEach(lck => {
+
+    lck.onclick = () =>{
+      lck.classList.toggle("fa-lock-open");
+      lck.classList.toggle("fa-lock");
+      lck.previousSibling.classList.toggle('active');
+      lck.parentNode.firstChild.classList.toggle('active');
+
+      if(lck.classList.contains('fa-lock')){
+        lck.style.opacity = "1";
+      }
+      else if(lck.classList.contains('fa-lock-open')){
+        lck.style.opacity = "0";
+      }
+    }
+  
+  });
+
+
+
+
+
+
+  
